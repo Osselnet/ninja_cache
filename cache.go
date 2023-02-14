7f5ff0b5bcee3280cdcefacec3164f90e7e5d156
Oleg Sergienko <hornets@list.ru>
2023-02-14 23:34:26 +0300

    corrected and added main

--- cache.go
+++ cache.go
@@ -1,23 +1,23 @@
-package cache
+package main
 
 type cache struct {
 	temple map[string]interface{}
 }
 
-func New() *cache {
+func NewCache() *cache {
 	return &cache{
 		temple: make(map[string]interface{}),
 	}
 }
 
-func (c cache) Set(key string, value interface{}) {
+func (c *cache) Set(key string, value interface{}) {
 	c.temple[key] = value
 }
 
-func (c cache) Get(key string) interface{} {
+func (c *cache) Get(key string) interface{} {
 	return c.temple[key]
 }
 
-func (c cache) Delete(key string) {
+func (c *cache) Delete(key string) {
 	delete(c.temple, key)
 }
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cache := NewCache()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId = cache.Get("userId")
+
+	fmt.Println(userId)
+}
