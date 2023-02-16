72513de94144831902b37ab56c1da7fba8af823f
Oleg Sergienko <hornets@list.ru>
2023-02-16 17:09:40 +0300

    add timeout

--- cache.go
+++ cache.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"time"
+	"errors"
+)
+
 type cache struct {
 	temple map[string]interface{}
 }
@@ -10,12 +15,22 @@ func NewCache() *cache {
 	}
 }
 
-func (c *cache) Set(key string, value interface{}) {
+func (c *cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.temple[key] = value
+	
+	go func() {
+		time.Sleep(ttl)
+		c.Delete(key)
+	}()
 }
 
-func (c *cache) Get(key string) interface{} {
-	return c.temple[key]
+func (c *cache) Get(key string) (interface{}, error) {
+	var err error
+	cacheValue := c.temple[key]
+	if cacheValue == nil {
+		err = errors.New("the key does not exist")
+	}
+	return cacheValue, err
 }
 
 func (c *cache) Delete(key string) {
--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 )
 
 func main() {
+	var err error
+
 	cache := NewCache()
 
-	cache.Set("userId", 42)
-	userId := cache.Get("userId")
+	cache.Set("userId", 42, time.Second * 5)
+	userId, _ := cache.Get("userId")	
 
 	fmt.Println(userId)
 
-	cache.Delete("userId")
-	userId = cache.Get("userId")
+	time.Sleep(time.Second * 6)
+
+	userId, err = cache.Get("userId")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(userId)
 }
